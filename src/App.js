1ed5b883996fd9b27a4265143f0273b631acce81
Jonathan Choi <jonchoi11@gmail.com>
2021-10-13 14:18:29 -0600

    add recipe api and use env for key protection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`;
+
   return (
     <div className="App">
-      <h1>What up</h1>
+      <h1>What Up!</h1>
     </div>
   );
-}
+};
 
 export default App;
 // adding a comment
