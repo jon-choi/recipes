eb9e4aefd6857ef288f09fa21fa303c2b8a38f37
Jonathan Choi <jonchoi11@gmail.com>
2021-10-15 15:55:22 -0600

    change position of image

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,13 +5,13 @@ const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div className={style.recipe}>
       <h1 className="recipe-title">{title}</h1>
+      <img className={style.image} src={image} alt="" />
       <ol>
         {ingredients.map((ingredient) => (
           <li className="font-style">{ingredient.text}</li>
         ))}
       </ol>
-      <p>{calories}</p>
-      <img className={style.image} src={image} alt="" />
+      <p>Calories: {calories}</p>
     </div>
   );
 };
